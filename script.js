d0f6014b9005e16acbb7a37f1bc4a605d74d7a2c
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-02 20:02:48 +0600

    hello

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const numbers = [15, 42, 8, 99, 23, 67];
-let findingBigNum = numbers.find(p => p >= 99)
-console.log(findingBigNum);
+//sob number er jogfol ber kora
+const numbers = [10, 20, 30, 40];
+let num = 0;
+for(let number of numbers){
+    num += number;
+}
+  console.log(num);
+
+
+// ami mone kori je amr modde potential to onk ase and ami ultimate potential ke meet korte parteci nah . accha what are way that i can truly meet my potential ami aita kibhave korbo airokom korar ki kono scientific way ase jodi thake tahole nijer ultimate potential level ke meet korbo kiabve amk no sugarcoat chara banglai bolba. 
+
+
 
 
