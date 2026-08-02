a115638b4634babb5748a618dddc76ac4a1de0ab
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-02 20:20:47 +0600

    hello

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//sob number er jogfol ber kora
-const numbers = [10, 20, 30, 40];
-let num = 0;
-for(let number of numbers){
-    num += number;
-}
-  console.log(num);
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.reverse());
+
+
 
 
 // ami mone kori je amr modde potential to onk ase and ami ultimate potential ke meet korte parteci nah . accha what are way that i can truly meet my potential ami aita kibhave korbo airokom korar ki kono scientific way ase jodi thake tahole nijer ultimate potential level ke meet korbo kiabve amk no sugarcoat chara banglai bolba. 
