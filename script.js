c2d1fcdc1b2d694c9f817e5a0197449f2720faf6
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-02 20:43:00 +0600

    hello

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 let isAnagram = ["listen", "silent"];
-
 let check1 = isAnagram[0].split("").sort().join("");
 let check2 = isAnagram[1].split("").sort().join("");
-
-console.log(check1);
-console.log(check2);
+console.log(check1 === check2);
 
 
 
@@ -12,7 +9,8 @@ console.log(check2);
 
 
 
-// ami mone kori je amr modde potential to onk ase and ami ultimate potential ke meet korte parteci nah . accha what are way that i can truly meet my potential ami aita kibhave korbo airokom korar ki kono scientific way ase jodi thake tahole nijer ultimate potential level ke meet korbo kiabve amk no sugarcoat chara banglai bolba. 
+
+
 
 
 
