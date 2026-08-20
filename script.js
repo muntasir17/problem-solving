6c59e01cf1d493c7ec6ad281dcfd65aac0a6c3c6
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-20 12:51:39 +0600

    hello

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let isAnagram = ["listen", "silent"];
-let check1 = isAnagram[0].split("").sort().join("");
-let check2 = isAnagram[1].split("").sort().join("");
-console.log(check1 === check2);
-
+let age = 20;
+console.log(age);
 
 
 
