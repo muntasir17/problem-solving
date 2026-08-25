fb7c3394397135421ec11c90fa04ba474f819d3c
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 10:27:53 +0600

    problem solved 6

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const products = [
-  { name: "Laptop", category: "Electronics" },
-  { name: "Shirt", category: "Clothing" },
-  { name: "Headphone", category: "Electronics" },
-  { name: "Book", category: "Education" },
-  { name: "Smartwatch", category: "Electronics" }
+const items = [
+  { name: "Bag", price: 1200 },
+  { name: "Pen", price: 20 },
+  { name: "Watch", price: 2500 },
+  { name: "Socks", price: 150 },
+  { name: "Jacket", price: 4500 }
 ];
-
-products.map((cat) =>{
-if(cat.category === "Electronics"){
-    console.log(cat.name);
-}
+items.filter((item)=>{
+    if(item.price > 500){
+        console.log(item.name);
+    }
 })
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
