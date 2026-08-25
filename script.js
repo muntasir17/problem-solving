102d9316bddf80eb14901ebceb4decf23faa8e8a
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 10:34:17 +0600

    problem solved 7

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-//নেক্সট টাস্ক: রেটিং ও স্টক অনুযায়ী সেরা বইয়ের নাম বের করা
 
-const books = [
-  { title: "The Alchemist", rating: 4.8, inStock: true },
-  { title: "Eat That Frog", rating: 4.2, inStock: true },
-  { title: "Ikigai", rating: 4.6, inStock: false },
-  { title: "Atomic Habits", rating: 4.9, inStock: true }
+const tasks = [
+  { title: "Learn JavaScript", isCompleted: true, priority: "High" },
+  { title: "Practice German Vocabulary", isCompleted: false, priority: "High" },
+  { title: "Clean Room", isCompleted: false, priority: "Low" },
+  { title: "Build Portfolio Project", isCompleted: false, priority: "High" }
 ];
-
-books.filter((item)=>{
-if(item.inStock === true && item.rating > 4.5){
+tasks.filter((item)=>{
+if(item.priority === "High" && item.isCompleted === false){
     console.log(item.title);
 }
 })
-
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
 
