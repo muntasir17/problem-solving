e4a7f64445e64802263764c44f5b1cb89ccdbf28
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 10:31:21 +0600

    problem solved 8

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const items = [
-  { name: "Bag", price: 1200 },
-  { name: "Pen", price: 20 },
-  { name: "Watch", price: 2500 },
-  { name: "Socks", price: 150 },
-  { name: "Jacket", price: 4500 }
+//নেক্সট টাস্ক: রেটিং ও স্টক অনুযায়ী সেরা বইয়ের নাম বের করা
+
+const books = [
+  { title: "The Alchemist", rating: 4.8, inStock: true },
+  { title: "Eat That Frog", rating: 4.2, inStock: true },
+  { title: "Ikigai", rating: 4.6, inStock: false },
+  { title: "Atomic Habits", rating: 4.9, inStock: true }
 ];
-items.filter((item)=>{
-    if(item.price > 500){
-        console.log(item.name);
-    }
+
+books.filter((item)=>{
+if(item.inStock === true && item.rating > 4.5){
+    console.log(item.title);
+}
 })
+
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
 
