c2372bb9555ea01c0456bded27a9816efd66a0af
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 09:52:45 +0600

    2nd problem solved

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//নিচে একটি প্রোডাক্ট ইনভেন্টরির অ্যারে দেওয়া আছে। তোমার কাজ হলো—যেসব প্রোডাক্ট স্টকে আছে (stock > 0), শুধু সেগুলোর নাম (name) কনসোলে প্রিন্ট করা।
-
-const inventory = [
-  { name: "Laptop", stock: 5 },
-  { name: "Mouse", stock: 0 },
-  { name: "Keyboard", stock: 12 },
-  { name: "Monitor", stock: 0 },
-  { name: "Headphone", stock: 8 }
+//নেক্সট টাস্ক: পাস করা শিক্ষার্থীদের নাম বের করা
+const students = [
+  { name: "Rahim", marks: 75 },
+  { name: "Karim", marks: 32 },
+  { name: "Sakib", marks: 88 },
+  { name: "Tamim", marks: 39 },
+  { name: "Riyad", marks: 60 }
 ];
-inventory.map((item)=>{
-if(item.stock > 0){
-console.log(item.name);
-}
+
+students.map((mark)=>{
+    if(mark.marks >= 40){
+        console.log(mark.name);
+    }
 })
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
+
+
