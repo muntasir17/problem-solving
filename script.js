37309ea7d590383a0a3dbfcc1882092d8d21c1fe
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 10:07:57 +0600

    problem solved 3

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-//নেক্সট টাস্ক: পাস করা শিক্ষার্থীদের নাম বের করা
-const students = [
-  { name: "Rahim", marks: 75 },
-  { name: "Karim", marks: 32 },
-  { name: "Sakib", marks: 88 },
-  { name: "Tamim", marks: 39 },
-  { name: "Riyad", marks: 60 }
-];
 
-students.map((mark)=>{
-    if(mark.marks >= 40){
-        console.log(mark.name);
+// total user number bair kora 
+const users = [
+  { name: "Arian", isActive: true },
+  { name: "Siam", isActive: false },
+  { name: "Nayeem", isActive: true },
+  { name: "Fahim", isActive: true },
+  { name: "Tanvir", isActive: false }
+];
+let count = 0;
+users.map((user)=>{
+    if(user.isActive === true){
+        count++;
     }
 })
+console.log("Total Active Users : " + count);
+
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
 
