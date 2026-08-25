cf8bd8b27b030650279349fd2a66c06852f73a17
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-25 10:37:45 +0600

    problem solved 9

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-
-const tasks = [
-  { title: "Learn JavaScript", isCompleted: true, priority: "High" },
-  { title: "Practice German Vocabulary", isCompleted: false, priority: "High" },
-  { title: "Clean Room", isCompleted: false, priority: "Low" },
-  { title: "Build Portfolio Project", isCompleted: false, priority: "High" }
+//নেক্সট টাস্ক: ইঞ্জিনিয়ারিং ডিপার্টমেন্টের মোট স্যালারি বের করা
+const employees = [
+  { name: "Siddik", department: "Engineering", salary: 50000 },
+  { name: "Rahim", department: "HR", salary: 35000 },
+  { name: "Tanvir", department: "Engineering", salary: 60000 },
+  { name: "Anik", department: "Marketing", salary: 40000 }
 ];
-tasks.filter((item)=>{
-if(item.priority === "High" && item.isCompleted === false){
-    console.log(item.title);
+let sum = 0;
+employees.filter((item)=>{
+if(item.department === "Engineering"){
+    sum += item.salary
 }
 })
+console.log(`Total Engineering Salary : ${sum}`);
 // ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
 
