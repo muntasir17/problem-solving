554d2e34792e0a4205f03b6c6df1f83d1a271109
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-26 17:43:55 +0600

    5th problem solved

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//লো-স্টক আইটেমগুলোর নাম বের করা
 
-const inventory = [
-  { name: "Notebook", stock: 15 },
-  { name: "Pen", stock: 3 },
-  { name: "Pencil", stock: 2 },
-  { name: "Eraser", stock: 20 },
-  { name: "Scale", stock: 4 }
+const customers = [
+  { name: "Rahim", spent: 1500, isVerified: true },
+  { name: "Karim", spent: 800, isVerified: true },
+  { name: "Sakib", spent: 2000, isVerified: false },
+  { name: "Tamim", spent: 1200, isVerified: true }
 ];
-inventory.filter((item)=>{
-    if(item.stock <= 5){
-        console.log(item.name);
+customers.filter((custo)=>{
+    if(custo.isVerified === true && custo.spent >= 1000){
+        console.log(custo.name);
     }
 })
-
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba.
