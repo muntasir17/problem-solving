5b4e7a6b6f577667b0262a86b860ec5342dbaf6d
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-26 17:39:51 +0600

    3rd problem solved

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-//নিচে একটি টিমের মেম্বারদের লিস্ট দেওয়া আছে। তোমার কাজ হলো—যেসব মেম্বারের রোল "Admin" এবং তারা বর্তমানে অ্যাক্টিভও আছে (isActive === true), শুধু সেগুলোর নাম (name) কনসোলে প্রিন্ট করা।
-const teamMembers = [
-  { name: "Siam", role: "Admin", isActive: true },
-  { name: "Rakib", role: "User", isActive: true },
-  { name: "Tanvir", role: "Admin", isActive: false },
-  { name: "Fahim", role: "Admin", isActive: true }
+//নিচে একটি ই-কমার্স শপের অর্ডারের লিস্ট দেওয়া আছে। তোমার কাজ হলো—যেসব অর্ডার সাকসেসফুলি পেইড হয়েছে (isPaid === true), সেগুলোর মোট টাকার পরিমাণ (amount) যোগ করে কনসোলে প্রিন্ট করা।
+
+const orders = [
+  { id: 101, amount: 1200, isPaid: true },
+  { id: 102, amount: 800, isPaid: false },
+  { id: 103, amount: 2500, isPaid: true },
+  { id: 104, amount: 450, isPaid: false }
 ];
-teamMembers.filter((member)=>{
-    if(member.role === "Admin" && member.isActive === true){
-        console.log(member.name);
-    }
+let sum = 0;
+orders.filter((item)=>{
+if(item.isPaid === true){
+    let totalBil = item.amount;
+    sum += totalBil
+}
 })
+console.log(`Total Paid Amount: ${sum}`);
+
+
+
+
+
+
+
+
+
+
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba.
