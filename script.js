2223d0457effc083e3e18a284acf018264d0f9f9
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-26 18:04:55 +0600

    10th problem solved

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-//নিচে কিছু অনলাইন অর্ডারের তালিকা দেওয়া আছে। তোমার কাজ হলো—যেসব অর্ডারের মোট অ্যামাউন্ট ১,০০০ টাকা বা তার বেশি (totalAmount >= 1000), সেগুলোর ID কনসোলে প্রিন্ট করা।
+// /নিচে একটি সোশ্যাল মিডিয়া অ্যাকাউন্টের কিছু পোস্টের লিস্ট দেওয়া আছে। তোমার কাজ হলো—যেসব পোস্ট পাবলিক করা আছে (isPublic === true), সেগুলোর মোট লাইকের সংখ্যা (likes) যোগ করে কনসোলে প্রিন্ট করা।
 
-const orders = [
-  { id: "ORD01", totalAmount: 1500 },
-  { id: "ORD02", totalAmount: 800 },
-  { id: "ORD03", totalAmount: 2200 },
-  { id: "ORD04", totalAmount: 450 }
+
+const posts = [
+  { title: "My First Day at Gym", likes: 120, isPublic: true },
+  { title: "Personal Notes", likes: 50, isPublic: false },
+  { title: "JavaScript Tips", likes: 350, isPublic: true },
+  { title: "Secret Recipe", likes: 90, isPublic: false }
 ];
 
-orders.filter((item)=>{
-    if(item.totalAmount >= 1000){
-        console.log(item.id);
-        
+let sum = 0;
+posts.filter((post)=>{
+    if(post.isPublic === true){
+        sum += post.likes;
     }
 })
-
-
-
-
-
-
-
-
+console.log(`Total Public Likes: ${sum}`);
 
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba.
