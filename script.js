f5488be6ad3f59e366118ab90211a61041f5aec8
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-26 17:49:37 +0600

    7th problem solved

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-const customers = [
-  { name: "Rahim", spent: 1500, isVerified: true },
-  { name: "Karim", spent: 800, isVerified: true },
-  { name: "Sakib", spent: 2000, isVerified: false },
-  { name: "Tamim", spent: 1200, isVerified: true }
+//নিচে কিছু প্রোডাক্টের তালিকা দেওয়া আছে। তোমার কাজ হলো—যেসব প্রোডাক্টের রেটিং ৪.০ বা তার বেশি (rating >= 4.0), সেগুলোর নাম এবং ১০% ডিসকাউন্ট দেওয়ার পর নতুন দাম কত হবে (price - (price * 0.10)), তা কনসোলে প্রিন্ট করা।
+const items = [
+  { name: "Shoes", price: 1000, rating: 4.5 },
+  { name: "Jacket", price: 2000, rating: 3.8 },
+  { name: "Watch", price: 1500, rating: 4.2 }
 ];
-customers.filter((custo)=>{
-    if(custo.isVerified === true && custo.spent >= 1000){
-        console.log(custo.name);
-    }
+items.filter((item) =>{
+if(item.rating >= 4.0){
+    let relaPrice = (item.price * 10) / 100;
+    let actualPrice = item.price - relaPrice;
+    console.log(`${item.name} - Discounted Price: ${actualPrice}`);
+}
 })
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba.
