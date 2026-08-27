7d26d96525d911292644337b7eb4ad14cdf04aec
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-27 14:28:09 +0600

    9th

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-const subscriptions = [
-  { user: "Nayeem", plan: "Premium", price: 500, isActive: true },
-  { user: "Fahim", plan: "Basic", price: 200, isActive: true },
-  { user: "Siam", plan: "Premium", price: 500, isActive: false },
-  { user: "Tanvir", plan: "Premium", price: 500, isActive: true }
+const rooms = [
+  { roomNo: "101", price: 2000, type: "Deluxe", isAvailable: true },
+  { roomNo: "102", price: 1500, type: "Standard", isAvailable: true },
+  { roomNo: "103", price: 2500, type: "Deluxe", isAvailable: false },
+  { roomNo: "104", price: 3000, type: "Deluxe", isAvailable: true }
 ];
-let sum = 0;
-subscriptions.filter((user)=>{
-    if(user.isActive === true && user.plan === "Premium"){
-        sum += user.price
+rooms.filter((room)=>{
+    if(room.type === "Deluxe" && room.isAvailable === true){
+let discount = (room.price * 20) / 100;
+let actualPrice = room.price - discount;
+console.log(`${room.roomNo} - Discounted Price : ${actualPrice}`);
     }
 })
-console.log(`Total Premium Revenue: ${sum}`);
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
