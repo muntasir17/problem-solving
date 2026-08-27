90be0df8c0c5d1ae706288c9fce866d123890e5d
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-27 14:03:38 +0600

    1st

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// /নিচে একটি সোশ্যাল মিডিয়া অ্যাকাউন্টের কিছু পোস্টের লিস্ট দেওয়া আছে। তোমার কাজ হলো—যেসব পোস্ট পাবলিক করা আছে (isPublic === true), সেগুলোর মোট লাইকের সংখ্যা (likes) যোগ করে কনসোলে প্রিন্ট করা।
 
-
-const posts = [
-  { title: "My First Day at Gym", likes: 120, isPublic: true },
-  { title: "Personal Notes", likes: 50, isPublic: false },
-  { title: "JavaScript Tips", likes: 350, isPublic: true },
-  { title: "Secret Recipe", likes: 90, isPublic: false }
+const products = [
+  { name: "Laptop", category: "Electronics", inStock: true },
+  { name: "Shirt", category: "Clothing", inStock: true },
+  { name: "Headphones", category: "Electronics", inStock: false },
+  { name: "Smartphone", category: "Electronics", inStock: true }
 ];
 
-let sum = 0;
-posts.filter((post)=>{
-    if(post.isPublic === true){
-        sum += post.likes;
+products.filter((item)=>{
+    if(item.inStock === true && item.category === "Electronics"){
+        console.log(item.name);
     }
 })
-console.log(`Total Public Likes: ${sum}`);
 
-//ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba.
+//ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
+
