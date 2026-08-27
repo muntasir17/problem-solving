1cda5b58f6280f212b5a7d9eb30e3c63fe19f55f
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-27 14:24:41 +0600

    8th

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const players = [
-  { name: "HeroX", score: 500, level: 12, isOnline: true },
-  { name: "Shadow", score: 300, level: 8, isOnline: true },
-  { name: "Viper", score: 700, level: 15, isOnline: false },
-  { name: "Blaze", score: 450, level: 10, isOnline: true }
-];
 
+const subscriptions = [
+  { user: "Nayeem", plan: "Premium", price: 500, isActive: true },
+  { user: "Fahim", plan: "Basic", price: 200, isActive: true },
+  { user: "Siam", plan: "Premium", price: 500, isActive: false },
+  { user: "Tanvir", plan: "Premium", price: 500, isActive: true }
+];
 let sum = 0;
-players.filter((player)=>{
-if(player.isOnline === true && player.level >= 10){
-    sum += player.score;
-}
+subscriptions.filter((user)=>{
+    if(user.isActive === true && user.plan === "Premium"){
+        sum += user.price
+    }
 })
-console.log(`Total Top Player Score: ${sum}`);
+console.log(`Total Premium Revenue: ${sum}`);
 //ai nao ami bair kore dilam and tomar output gula o bair hoise akn tumi amk bolte parba nah je aita airokom hobe nah onno bhave hobe hen ten aigula amr kaj hocce bair kora aita jairokom bhave hok. jodi amr problem thik hoye thake tahole amk one line e akta verdict diye next problem diba. 
 
