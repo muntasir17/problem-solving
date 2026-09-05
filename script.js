50e0a5b5721bcc4e760b0b9c2e6f327245029a90
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-05 14:45:55 +0600

    3rd

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const items = [
-  { name: "Headphone", price: 3000, discountPercent: 10 },
-  { name: "Smart Watch", price: 5000, discountPercent: 0 },
-  { name: "Keyboard", price: 2000, discountPercent: 20 }
+const cartItems = [
+  { name: "Shirt", price: 100, weightInKg: 0.5 },
+  { name: "Shoes", price: 3500, weightInKg: 1.2 },
+  { name: "Socks", price: 20, weightInKg: 0.1 }
 ];
-items.map((item)=>{
-    let actualNumber = (item.price * item.discountPercent) / 100
-    let realPrice = item.price - actualNumber;
-    if(item.discountPercent > 0){
-console.log(`${item.name}. Final Price : ${realPrice} (${item.discountPercent} % OFF)`);
-    }else{
-        console.log(`${item.name}. Final Price : ${realPrice} Tk `);
-    }
+let totalPrice = 0;
+let totalWeight = 0
+cartItems.map((item)=>{
+    totalPrice += item.price;
+   totalWeight += item.weightInKg;
 })
+if(totalPrice >= 4000){
+    console.log(`Shipping Fee: 0 Tk (Free Shipping)`);
+}else{
+   let shippingFee = 80 + (totalWeight * 30);
+   totalPrice += shippingFee;
+   console.log(`Grand Total: ${totalPrice} Tk`);
+}
+
+ console.log(`Total Product Price: ${totalPrice} Tk`);
+ console.log(`Total Weight: ${totalWeight} Kg`);
+
+
+
+
 
