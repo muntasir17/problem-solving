a711a0350509c18031cafba0ca7e77a3f367d2c5
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-05 15:51:26 +0600

    6th

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const user = { name: "Anik", role: "editor" };
-
-const menuItems = [
-  { title: "Dashboard", allowedRoles: ["admin", "editor", "user"] },
-  { title: "User Management", allowedRoles: ["admin"] },
-  { title: "Content Editor", allowedRoles: ["admin", "editor"] },
-  { title: "System Settings", allowedRoles: ["admin"] },
-  { title: "My Profile", allowedRoles: ["admin", "editor", "user"] }
+const products = [
+  { name: "Samsung Galaxy S23", category: "Electronics", price: 70000, inStock: true },
+  { name: "iPhone 15 Pro", category: "Electronics", price: 130000, inStock: false },
+  { name: "Wireless Mouse", category: "Accessories", price: 1500, inStock: true },
+  { name: "Mechanical Keyboard", category: "Accessories", price: 4500, inStock: true },
+  { name: "Xiaomi Redmi Note", category: "Electronics", price: 25000, inStock: true }
 ];
-console.log(`Accessible Menus for ${user.name} (${user.role}):`);
-menuItems.map((item)=>{
-if(item.allowedRoles.includes(user.role) ){
-    console.log(`- ${item.title}`);
+console.log(`Matching Products:`);
+products.map((item)=>{
+if(item.category === "Electronics" && item.price <= 80000 && item.inStock === true){
+console.log(`- ${item.name} (Price: ${item.price} Tk)`);
 }
 })
-
-
+// bro guess what i completed all the task and perfectly assigned. akn to ar bolte parba nah amr code e kono bhul ase. aita ami damn sure je 100% hoise. 
 
 
 
