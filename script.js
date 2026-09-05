9e6afcb105df227639c5f666271fbf1af584152d
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-05 16:36:20 +0600

    8th

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const customer = {
-  name: "Sumi",
-  totalSpent: 25000,
-  ordersCount: 6,
-  currentPurchase: 5000
+const checkoutData = {
+  cartSubtotal: 3200,
+  couponCode: "SAVE20",
+  userBalance: 3000
 };
-console.log(`Customer : ${customer.name}`);
+console.log(`Checkout Details:`);
+console.log(`- Subtotal: ${checkoutData.cartSubtotal} Tk`);
 
-if(customer.totalSpent >= 50000 || customer.ordersCount >= 10){
-console.log(`Loyalty Rank: VIP`);
-}else if(customer.totalSpent >= 20000 && customer.ordersCount >= 5){
-console.log(`Loyalty Rank: Gold 🥇`);
-}else if(customer.totalSpent >= 5000){
-  console.log(`Loyalty Rank: Silver`);
-}else{
-    console.log(`Loyalty Rank: Bronze`);
+if(checkoutData.couponCode === "SAVE20"){
+let result = (checkoutData.cartSubtotal * 20) / 100;
+let finalPrice = checkoutData.cartSubtotal - result;
+console.log(`- Discount : ${result} Tk (${checkoutData.couponCode} applied)`);
+if(checkoutData.userBalance >= finalPrice){
+  console.log(`- Payment Status: Order Placed Successfully! ✅`);
 }
-console.log(`Customer : ${customer.currentPurchase}`);
-let points = (customer.currentPurchase / 100) * 2;
-console.log(`Earned Points: ${points} Points (Double Bonus Applied!)`);
+let remainingBalance = checkoutData.userBalance - finalPrice;
+console.log(`- Remaining Wallet Balance: ${remainingBalance} Tk`);
+}
+// aikhane ami - Shipping Fee: 0 Tk (Free Shipping) chara baki sob concept ami milay felsi asa kori. and aita oto important mone hocce nah. ami asol logic milay felsi and aita amr jonno enough ajke ato problem solve korlam ato happy ami every problem e ami onk kicu shiklam. thank you tomake amk airokom problem dewar jonno. amk ki arekta problem dewa jabe. 
 
-// bro aita teo matha khataite hoise that means aitao akta bhalo level er problem silo and ami asa korbo tumi aitao 100% parchi ai bheve dhore niba. and amk next problem diba. 
+
+
+
+
+
+
+
+
+
