8711c108fdacbfa031240df887ad00d73a0b69e5
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-07 11:39:19 +0600

    7th

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let totalQuantity = 0
-const cart = [
-  { name: "T-Shirt", price: 300, quantity: 2 },
-  { name: "Jeans", price: 800, quantity: 1 },
-  { name: "Socks", price: 50, quantity: 3 }
+const products = [
+  { name: "Wireless Mouse", price: 25, inStock: true, rating: 4.5 },
+  { name: "Gaming Keyboard", price: 80, inStock: false, rating: 4.8 },
+  { name: "USB Cable", price: 10, inStock: true, rating: 3.8 },
+  { name: "Headphones", price: 50, inStock: true, rating: 4.2 }
 ];
-cart.forEach((item)=>{
-  totalQuantity += item.quantity
+let arr = []
+products.forEach((item)=>{
+  if(item.inStock === true && item.rating >= 4.0){
+    arr.push(item.name)
+  }
 })
-console.log(totalQuantity);
-
+console.log(arr);
 
 
